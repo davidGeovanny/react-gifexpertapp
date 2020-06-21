2f00d7193a1420bf4d016e6fddd4f82e67f87d0c
David Geovanny <david.geovanny.cr@gmail.com>
2020-06-20 22:32:02 -0600

    docs agregado

---
+++ docs/precache-manifest.4786da4aba82fb83853f82985c65e1f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ad3c1ff5a0c167812bce702cb0f977",
+    "url": "/index.html"
+  },
+  {
+    "revision": "064ce8d99ff1df4c27a4",
+    "url": "/static/css/main.22063b4d.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064ce8d99ff1df4c27a4",
+    "url": "/static/js/main.6b621d43.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
